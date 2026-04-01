73ff0980e55423e5c3106f4d0027608f17fca4a3
Dishant Gotis <dishantgotisdg9881@gmail.com>
2026-04-02 02:39:39 +0530

    changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 import AboutSection from "@/components/ui/about-section";
+import { AnimatedTestimonialsDemo } from "@/components/ui/animated-testimonials-demo";
 
 export default function AboutPage() {
   return (
     <div className="min-h-screen w-full bg-black text-white">
       <AboutSection />
+      <AnimatedTestimonialsDemo />
     </div>
   );
 }
